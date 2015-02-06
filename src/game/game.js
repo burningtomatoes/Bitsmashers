d216c323aa5e234f4256d472680e1cf854074e77
Roy de Jong <roy@softwarepunt.nl>
2015-02-06 21:07:40 +0100

    [Project] Initialize. Hello world.

---
+++ src/game/game.js
@@ -0,0 +1,17 @@
+var Game = {
+    buildCode: 1000,
+
+    init: function () {
+        console.info('[Game] Initializing BitSmashers R' + this.buildCode + '...');
+
+        Canvas.init();
+    },
+
+    draw: function (ctx) {
+
+    },
+
+    update: function () {
+
+    }
+};
